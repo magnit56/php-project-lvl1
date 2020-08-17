8b1b1cf24ec5066d4bfcb9c6ad886a612a27279f
Samat Tulegenov <magnit56@gmail.com>
2020-08-17 20:27:10 +0300

    added engine

--- src/brain-even.php
+++ src/brain-even.php
@@ -26,29 +26,105 @@ function Run()
     }
 }
 
-function hello()
+//function oldHello()
+//{
+//    line('Welcome to the Brain Game!');
+//    line('Answer "yes" if the number is even, otherwise answer "no".');
+//    $name = prompt('May I have your name?');
+//    line("Hello, %s!", $name);
+//    return $name;
+//}
+
+//function question()
+//{
+//    $number = rand(1, 100);
+//    line("Question: %s", $number);
+//    return $number;
+//}
+
+//function userAnswer()
+//{
+//    $userAnswer = prompt('Your answer');
+//    return $userAnswer;
+//}
+
+
+
+
+
+
+
+
+
+
+
+function printWelcome()
 {
-    line('Welcome to the Brain Game!');
-    line('Answer "yes" if the number is even, otherwise answer "no".');
+    line("Welcome to the Brain Game!");
+}
+
+function getGameTitle($gameTitle)
+{
+    return $gameTitle;
+}
+
+function printGameTitle()
+{
+    $gameTitle = getGameTitle();
+    if ($gameTitle == "even") {
+        line("Answer \"yes\" if the number is even, otherwise answer \"no\".");
+    } elseif ($gameTitle == "calc") {
+        line("What is the result of the expression?");
+    } elseif ($gameTitle == "gcd") {
+        line("Find the greatest common divisor of given numbers.");
+    }
+}
+
+function setName()
+{
     $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
     return $name;
 }
 
-function question()
+function getName()
+{
+    return $name;
+}
+
+function hello()
+{
+    $name = getName();
+    line("Hello, %s!", $name);
+}
+
+function cycle()
+{
+    $i = 3;
+    $correct = true;
+    while ($i > 0 && $correct) {
+
+    }
+}
+
+function getQuestion()
 {
     $number = rand(1, 100);
     line("Question: %s", $number);
     return $number;
 }
 
-function userAnswer()
+function setAnswer()
 {
     $userAnswer = prompt('Your answer');
     return $userAnswer;
 }
 
-function rightAnswer($number)
+function getAnswer($userAnswer)
+{
+    return $userAnswer;
+}
+
+function getCorrectAnswer($number)
 {
     if ($number % 2 == 0) {
         return 'yes';
---
+++ src/run.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BrainGames\Run;
+
+use function cli\line;
+use function cli\prompt;
+
+const ROUNDS_COUNT = 3;
+
+function run($getData, $gameTask)
+{
+    line('Welcome to the Brain Game!');
+    line("%s", $gameTask);
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    for ($roundsCount = 0; $roundsCount < ROUNDS_COUNT; $roundsCount++) {
+        $data = $getData;
+        $question = $data['question'];
+        $correctAnswer = $data['correctAnswer'];
+        line("Question: %s", $question);
+        $userAnswer = prompt("Your answer: ");
+        if ($userAnswer == $correctAnswer) {
+            line("Correct!");
+        } else {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $userAnswer, $correctAnswer);
+            line("Let's try again, %s!", $name);
+            exit;
+        }
+        line("Congratulations, %s!", $name);
+    }
+}
