a98033b99b9ff18743e6b58db48b13aaad447772
Samat Tulegenov <magnit56@gmail.com>
2020-08-19 02:12:07 +0300

    added game Prime

---
+++ src/games/prime.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BrainGames\Games\Prime;
+
+use function BrainGames\Run\run;
+
+function isPrime($number)
+{
+    if (gmp_prob_prime($number) == 0) {
+        return 'no';
+    }
+    if (gmp_prob_prime($number) == 2) {
+        return 'yes';
+    }
+}
+
+function runPrime()
+{
+    $getData = function () {
+        $question = rand(1, 99);
+        $correctAnswer = isPrime($question);
+        $data = [];
+        $data['question'] = $question;
+        $data['correctAnswer'] = $correctAnswer;
+        return $data;
+    };
+    $gameTask = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    run($getData, $gameTask);
+}
