f28fe6de9c851f490099ae620f329d358221f49c
Samat Tulegenov <magnit56@gmail.com>
2020-09-10 19:43:24 +0300

    added const progression count instead magic number

--- src/games/progression.php
+++ src/games/progression.php
@@ -4,13 +4,15 @@ namespace BrainGames\Games\Progression;
 
 use function BrainGames\Run\run;
 
+const PROGRESSION_COUNT = 10;
+
 function runProgression()
 {
     $getData = function () {
         $firstItem = rand(1, 9);
         $step = rand(1, 10);
         $items = [];
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < PROGRESSION_COUNT; $i++) {
             $items[$i] = $firstItem + $i * $step;
         }
         $riddle = rand(1, 10);
